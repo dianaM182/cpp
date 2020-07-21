90804e9a19a4a2322b484c130909cfaf31c48343
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-07-20 19:51:25 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio16-ciclo while.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	using namespace std;
+	
+	int a=5;
+	int b=2;
+	
+	while(a>b){
+		
+		cout<<"Ingrese el valor de a"<<endl;
+		cin>>a;
+			
+	}
+	cout<<"Saliste del clico";
+	cout<<endl;
+		return 0;
+}
