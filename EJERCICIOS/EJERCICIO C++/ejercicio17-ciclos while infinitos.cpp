ceb85ffb3ee39150f338a246c367923eacdc4cac
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-07-20 19:59:51 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio17-ciclos while infinitos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	using namespace std;
+	int ciclos=0;
+	
+
+	while(ciclos<100){
+		cout<<"*";
+		ciclos=ciclos + 1;
+	}
+	return 0;
+}
