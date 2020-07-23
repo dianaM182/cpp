6d01368a04db2d3f38d178461a88cff07f76b9ae
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-07-22 18:00:15 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio22-ciclos for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	using namespace std;
+	
+	for(int indice=1; indice<=10; indice ++)
+	{
+		cout<<"valor de la variable del indice"<<indice<<endl;
+	}
+	return 0;
+}
