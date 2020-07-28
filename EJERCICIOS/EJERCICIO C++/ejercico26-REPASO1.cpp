f27fc38b13d303b6ee80d1d93fae2a35fddc9d26
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-07-27 21:28:56 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercico26-REPASO1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	using namespace std;
+	
+	for (int i=1; i<=10; i++ )
+	{
+		cout<<"Indice:"<<i;
+		cout<<endl;
+	}
+	
+	cout<<endl;
+	cout<<endl;
+	
+	int i=1;
+	while(1<=10){
+		cout<<"Indice:"<<i;
+		cout<<endl;
+		i++;
+	}
+	
+	return 0;
+}
