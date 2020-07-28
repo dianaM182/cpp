ea1aaa03cc1b57d9329241012e9371c890e5a159
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-07-27 21:30:31 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio28-funciones.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int sumar(){
+	return 5+7;
+}
+
+string nombrecompleto(){
+  string nombre="juan" ;
+  string apelledio="perez";	
+}
+
+int main(int argc, char** argv) {
+
+ int resultado=sumar();
+ string nombreyapellido=nombrecompleto();
+ 
+ 
+ cout<<"Resultado";
+ cout<<endl;
+ cout<<"nombre y apellido:";
+	
+	
+	return 0;
+}
