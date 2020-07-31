84d093b53f67d4afd401f1bb1351daac31664e6b
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-07-31 17:15:04 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio35-funciones con ciclos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+void imprimircaracteres(char caracteres, int ciclos){
+	
+	for(int i=0; i<ciclos; i++){
+		cout<<"cararteres";
+		}
+		cout<<endl;
+}
+int main(int argc, char** argv) {
+	
+	imprimircaracteres('@',10);
+	imprimircaracteres('#',5)
+	cout<<"Hola"<<endl;
+	imprimircaracteres('*',1000);
+	return 0;
+}
