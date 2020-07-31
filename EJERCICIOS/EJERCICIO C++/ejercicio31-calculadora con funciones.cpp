d4557e0f6de431ce5f6508e5b677954433d223fb
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-07-31 14:25:00 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio31-calculadora con funciones.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ using namespace std;
+  sumar (int a, int b){
+  	return a+b;
+  }
+  
+ int calculadora(int a, int b, char operador){
+ 	if (operador== '+'){
+ 		return sumar(a,b);
+	 } 
+	 return -1;
+ }
+int main(int argc, char** argv) {
+	
+	int n1=0;
+	int n2=0;
+	char operador=0;
+	
+	cout<<"ingrese n1: ";
+	cin>>n1;
+	
+		cout<<"ingrese n2: ";
+	cin>>n2;
+	cout<<"Ingrese el operador (+ - * /): ";
+	cin>> operador;
+	cout<<endl;
+	cout<<"Resultado " << n1 << operador << n2 <<" es:";
+	cout<<calculadora(n1,n2,operador);
+	
+	return 0;
+}
