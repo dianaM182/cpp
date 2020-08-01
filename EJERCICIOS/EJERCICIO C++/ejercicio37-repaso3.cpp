f3f00703ef3643bbd96d28e078ff8352456591c0
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-07-31 20:38:52 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio37-repaso3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int a=5;
+	int b=10;
+	int c=4;
+	
+//	if(a>b && c>b){
+		
+	//	cout<<"a es mayor que b o  mayor  c";	
+	}
+	
+//	while(a>b){
+//		a--;
+//		if(a==20){
+//			break;
+//		}
+//	}
+	
+	while(true){
+		while(true){
+			if(a==10){
+				break;
+			}
+			b++;
+		}
+		a++;
+		    if(a==1){
+				break;
+			}
+			a++;
+	}
+	return 0;
+}
