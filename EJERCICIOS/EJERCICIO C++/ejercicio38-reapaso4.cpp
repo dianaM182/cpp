5ead26cfa2a0e7d8565ec89faf3220060bf6a08e
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-08-01 17:44:49 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio38-reapaso4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv[]) {
+	
+	system("cls")
+	for (int i=5; i<=100; i=i+5) 
+	{
+		cout<<i <<endl;
+		
+		for(int j=1; j<=5; j++)
+		{
+		cout<<j <<endl;
+     	} 
+     	
+     		for(int k=1; k<=3; k++)
+			 {
+	    	cout<<k <<endl;
+        	}
+        	
+        		if(i==25)
+		      {
+			 break;
+	     	 }
+	}
+	
+//	int i=5;
+//	while(i<=100){
+//		cout<<i << endl;
+//		i=i+5;
+//		
+//		if(i==25)
+//		{
+//			break;
+//		}
+//		
+//	}
+	return 0;
+}
