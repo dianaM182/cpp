fb2c161764b2b4e9ab62b648c095cdf808d6007e
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-08-02 22:33:47 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio42-punto de venta parte III/factura.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+double subtotal;
+
+string ListaProductos;
+void agregarproducto (string descripcion, int cantidad, double precio){
+	
+	ListaProductos=ListaProductos+descripcion+'\n';
+	subtotal=subtotal+(cantidad*precio)
+}
+
+
+
+
+
---
+++ EJERCICIOS/EJERCICIO C++/ejercicio42-punto de venta parte III/productos.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+ using namespace std;
+ 
+ extern void agregarproducto (string descripcion, int cantidad, double precio);
+ void  producto(int opcion)
+ {
+    system("cls");
+     int opcionproducto=0;
+     switch (opcion)
+     {
+     case 1:
+      {
+     	
+        cout<<"BEBIDAS CALIENTES"<<endl;
+        cout<<"*****************"<<endl;
+        cout<<"1.Capucino"<<endl;
+        cout<<"2.Expreso"<<endl;
+        cin>>opcionproducto;
+        
+     	switch(opcionproducto)
+			 {
+     			case 1:
+     			agregarproducto	("Capuchino",1, 40);
+     			break;
+     			
+     			case 2:
+     			agregarproducto	("Expreso",1, 30);
+     			break;
+     			default:
+     				cout<<"opcion no valida";
+     		    break;
+			 }	
+		 }
+         break; 
+     }
+     
+      case 2:
+     {
+     cout<<"**BEBIDAS FRIAS**"<<endl;
+     cout<<"*****************"<<endl;
+     system("pause");
+         break;
+     }
+
+     case  3:
+     {
+     cout<<"***REPOSTERIA***"<<endl;
+     cout<<"****************"<<endl;
+     system("pause");
+         break;
+     }
+     default:
+         break;
+     }
+ }
