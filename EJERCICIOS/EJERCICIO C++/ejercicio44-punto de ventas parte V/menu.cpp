16f08c192d9d85d72cf0e242b82dc09a4efb4ead
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-08-03 20:09:52 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio44-punto de ventas parte V/menu.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+ using namespace std;
+ 
+extern void producto(int opcion);
+extern void imprimirfactura();
+
+ void menu(){
+ 	int opcion=0;
+	while(true)
+	{
+		
+		system("cls");
+		cout<<"********";
+		cout<<"**Menu**";
+		cout<<"********";
+		
+		cout<<endl;
+		cout<<endl;
+		
+		cout<<"1.BEBIDAS CALIENTES"<<endl;
+		cout<<"2.BEBIDAS FRIAS"<<endl;
+		cout<<"3.REPOSTERIA"<<endl;
+		cout<<"4.Imprimir factura"<<endl;
+		cout<<"0.SALIR"<<endl;
+		
+		cout<<endl;
+		cout<<"ingrese una opcion:";
+		cin>>opcion;
+		
+		if(opcion == 0)
+		{ 
+			
+			break;
+		}
+	    
+		
+	     if(opcion==4)
+		 {
+	    	imprimirfactura();
+	    	cout<<"si ombe";
+	  
+	   	}  
+			else
+			{
+			producto(opcion);
+		//	imprimirfactura();
+	   		 }
+		producto(opcion);
+	}
+	   
+}
