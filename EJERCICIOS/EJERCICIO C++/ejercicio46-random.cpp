bef3d39db9a8f166fd26f54c79b9067bd7351399
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-08-06 20:10:42 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio46-random.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv)
+ {
+	int  numero=0;
+	
+	//INICALIZA EL NUMERO RAMDON
+	srand(time(NULL));
+	
+	for(int i=0; i<20; i++)
+	{
+		
+	 //genera un numero entre 1 y 10
+	 numero=rand()%20+1;
+	 cout<<" Numero al azar: " << numero<<endl;		
+	}
+			
+	return 0;
+}
