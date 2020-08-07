12675168c2569b536285cef7585889086dab74f2
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-08-06 19:34:30 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio45-archivos h/alumnos.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+ 
+ using namespace std;
+ 
+ string nombre = "juan";
+ string Apellido = "perez";
+ int edad = 18;
+ 
+ string ObtenerNombreCompleto() {
+ 	return nombre + " " + Apellido;
+ }
+ 
+ int Obteneredad(){
+ 	return edad;
+ 	
+ }
---
+++ EJERCICIOS/EJERCICIO C++/ejercicio45-archivos h/main.cpp
@@ -0,0 +1,15 @@
+  #include <iostream>
+#include "calculadora.h"
+#include "alumnos.h"
+
+using namespace std;
+
+int main(int argc, char const * argv []) 
+{
+	cout<<calcular(5,7,'+');
+	cout<<endl;
+	cout<< ObtenerNombreCompleto();
+	cout<< endl;
+	cout<< Obteneredad();
+	return 0;
+}
