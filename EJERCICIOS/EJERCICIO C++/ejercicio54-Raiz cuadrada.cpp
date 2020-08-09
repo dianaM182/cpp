6f65e69a30a30d498bd0840345f5cfc63dd3820c
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-08-08 18:18:57 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/ejercicio54-Raiz cuadrada.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+#define Pi 31416
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	float a,b;
+	
+	cout<<"RAIZ CUADRADA"<<endl;
+	cout<<endl;
+	cout<<"Ingrese un numero:";
+	cin>>a;
+	b=sqrt(a);
+	
+	cout<<"El resultado es:"<<b<<endl;
+	return 0;
+	system("pause");
+}
