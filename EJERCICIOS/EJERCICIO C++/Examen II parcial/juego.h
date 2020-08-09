8c8da53cdbdaf3008f3a353f11ad58c233f86f40
dianaM182 <67715557+dianaM182@users.noreply.github.com>
2020-08-09 17:02:59 -0600

    Add files via upload

---
+++ EJERCICIOS/EJERCICIO C++/Examen II parcial/juego.h
@@ -0,0 +1,13 @@
+  
+void  inicializarArreglo ();
+void dificultad ();
+ void pintar ();
+bool  gameover ();
+void  proceso ( char & tecla, int & puntos);
+ void pintar ();
+void  dibujarcuerpo ();
+void guardarposicion  ();
+void borrarcuerpo  ();
+void comida  ();
+void puntosx  ();
+void gotoxy ( int x, int y);
